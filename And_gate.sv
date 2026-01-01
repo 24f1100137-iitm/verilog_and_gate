993c90927e8db1b01f333cb7bfaf78b5e70c6a66
24f1100137-iitm <24f1100137@es.study.iitm.ac.in>
2026-01-01 15:21:12 +0530

    Update And_gate.sv

--- And_gate.sv
+++ And_gate.sv
@@ -1,1 +1,8 @@
-
+# And_gate design.sv in EDA playground
+// Code your design here
+module And(
+  input wire x,y,
+  output wire z
+);
+  assign z=x&y; // assign the value of x and y in z //
+endmodule
